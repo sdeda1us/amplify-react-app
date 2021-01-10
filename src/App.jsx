4bd3fbb63fa0daf9608d298030dcdb84d6e7baa6
sdeda1us <steven.maloney@gmail.com>
2021-01-10 09:17:40 -0600

    play button for audio logs s3 access url

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { listSongs } from './graphql/queries';
-import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import Amplify, { API, graphqlOperation, Storage } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import { Paper, IconButton } from '@material-ui/core';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import { updateSong } from './graphql/mutations';
+import PauseIcon from '@material-ui/icons/Pause';
 
 Amplify.configure(awsconfig);
 
 function App() {
   const [songs, setSongs] = useState([]);
+  const [songPlaying, setSongPlaying] = useState('');
+  const [audioURL, setAudioURL] = useState('');
+
 
   const fetchSongs = async () => {
       try {
@@ -45,6 +49,26 @@ function App() {
     }
   };
 
+  const toggleSong = async idx => {
+    if (songPlaying === idx) {
+        setSongPlaying('');
+        return;
+    }
+
+    const songFilePath = songs[idx].filePath;
+    try {
+        const fileAccessURL = await Storage.get(songFilePath, { expires: 60 });
+        console.log('access url', fileAccessURL);
+        setSongPlaying(idx);
+        setAudioURL(fileAccessURL);
+        return;
+    } catch (error) {
+        console.error('error accessing the file from s3', error);
+        setAudioURL('');
+        setSongPlaying('');
+    }
+};
+
   return (
     <>
     <header className="App-header">
@@ -56,8 +80,8 @@ function App() {
         return (
             <Paper variant="outlined" elevation={2} key={`song${idx}`}>
                 <div className="songCard">
-                    <IconButton aria-label="play">
-                        <PlayArrowIcon />
+                    <IconButton aria-label="play" onClick={() => toggleSong(idx)}>
+                      {songPlaying === idx ? <PauseIcon /> : <PlayArrowIcon />}
                     </IconButton>
                     <div>
                         <div className="songTitle">{song.title}</div>
